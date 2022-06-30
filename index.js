3ed734d8210f190194f2b68c3b27f8c2fc9834f1
Luca Viggiani <lviggiani@gmail.com>
2022-06-30 12:18:08 +0200

    completed module

---
+++ index.js
@@ -0,0 +1,40 @@
+import path from "path";
+import fs from "fs/promises";
+import HttpStatusCodes from "http-status-codes";
+import sass from "sass";
+
+const rendered = {},
+      watching = {};
+
+const watchFile = async file => {
+    if (watching[file]) return;
+
+    watching[file] = fs.watch(file);
+    for await (const e of watching[file])
+        if (e.eventType === "change") delete rendered[file];
+}
+
+export default (cssFolder = "css", scssFolder) => {
+
+    scssFolder = scssFolder || path.join(process.cwd(), "scss");
+
+    return async (req, res, next) => {
+        // ignore non css files
+        if (!req.path.endsWith(".css")) return next();
+    
+        const fileName = path.join(scssFolder, req.path.replace(cssFolder, ""))
+            .replace(/\.css$/, ".scss");
+
+        if (!await fs.stat(fileName).catch(_ => undefined)) return res.status(HttpStatusCodes.NOT_FOUND).end();
+
+        rendered[fileName] = rendered[fileName] || await sass.compileAsync(fileName).catch(err => undefined);
+
+        if (rendered[fileName]){
+            res.header("content-type", "text/css");
+            res.send(rendered[fileName].css.toString());
+            rendered[fileName].loadedUrls.map(url => url.pathname).forEach(watchFile);
+        } else {
+            res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).end();
+        }
+    }
+}
